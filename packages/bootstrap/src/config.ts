1bf8316cb711edb3a15700337209b7e2d79cad4c
Matthias von Bargen <matt@mavon.ie>
2026-03-28 16:17:10 +0100

    up version and tag reference

--- packages/bootstrap/src/config.ts
+++ packages/bootstrap/src/config.ts
@@ -1,4 +1,4 @@
 export const TAG_URL =
-  'https://github.com/MavonEngine/Core/archive/refs/tags/0.0.6-alpha.zip'
+  'https://github.com/MavonEngine/Core/archive/refs/tags/0.0.7-alpha.zip'
 
 export const TEMPLATE_SUBPATH = 'packages/multiplayer-template'
