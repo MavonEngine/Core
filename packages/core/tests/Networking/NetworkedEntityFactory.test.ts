9aec5139cf55f97c00358bef6752ce922560fc59
Matthias von Bargen <matt@mavon.ie>
2026-04-07 19:09:45 +0200

    fix test

--- packages/core/tests/Networking/NetworkedEntityFactory.test.ts
+++ packages/core/tests/Networking/NetworkedEntityFactory.test.ts
@@ -4,8 +4,8 @@ import NetworkedEntityFactory from '../../src/Networking/NetworkedEntityFactory'
 
 class TestActor extends NetworkedActor {
   $typeName = 'testActor'
-  updateFromNetwork = (_data: object) => {}
-  update(_delta: number): void {}
+  updateFromNetwork = (_data: object) => { }
+  update(_delta: number): void { }
 }
 
 describe('networkedEntityFactory', () => {
@@ -16,32 +16,32 @@ describe('networkedEntityFactory', () => {
   })
 
   it('returns null for an unregistered type', () => {
-    expect(factory.create('unknown', 'id-1', {})).toBeNull()
+    expect(factory.create('unknown', { id: 'id-1' })).toBeNull()
   })
 
   it('creates a registered entity', () => {
-    factory.register('testActor', (id, _data) => {
+    factory.register('testActor', (data) => {
       const actor = new TestActor()
-      actor.id = id
+      actor.id = data.id as string
       return actor
     })
 
-    const actor = factory.create('testActor', 'id-1', {})
+    const actor = factory.create('testActor', { id: 'id-1' })
     expect(actor).toBeInstanceOf(TestActor)
   })
 
   it('passes id and data to the factory function', () => {
-    const data = { x: 10, y: 20 }
+    const data = { x: 10, y: 20, id: 'id-42' }
     let capturedId: string | undefined
     let capturedData: Record<string, unknown> | undefined
 
-    factory.register('testActor', (id, d) => {
-      capturedId = id
-      capturedData = d
+    factory.register('testActor', (data) => {
+      capturedId = data.id as string
+      capturedData = data
       return new TestActor()
     })
 
-    factory.create('testActor', 'id-42', data)
+    factory.create('testActor', data)
     expect(capturedId).toBe('id-42')
     expect(capturedData).toEqual(data)
   })
@@ -65,8 +65,8 @@ describe('networkedEntityFactory', () => {
   it('handles multiple registered types independently', () => {
     class OtherActor extends NetworkedActor {
       $typeName = 'otherActor'
-      updateFromNetwork = (_data: object) => {}
-      update(_delta: number): void {}
+      updateFromNetwork = (_data: object) => { }
+      update(_delta: number): void { }
     }
 
     factory.register('testActor', () => new TestActor())
