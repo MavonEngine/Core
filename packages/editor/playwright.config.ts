3807a1fea8bbbdd704947522b4cc4124e87aca7d
Matthias von Bargen <matt@mavon.ie>
2026-04-29 11:27:45 +0200

    revert to dev mode as editor only loads in dev

--- packages/editor/playwright.config.ts
+++ packages/editor/playwright.config.ts
@@ -1,20 +1,17 @@
 import process from 'node:process'
 import { defineConfig, devices } from '@playwright/test'
 
-const CI = !!process.env.CI
-
 export default defineConfig({
   testDir: './e2e',
+  timeout: 60_000,
   webServer: {
-    command: CI
-      ? 'npm run preview --prefix ../multiplayer-template/client'
-      : 'npm run dev --prefix ../multiplayer-template/client',
-    url: CI ? 'http://localhost:4173' : 'http://localhost:5173',
-    reuseExistingServer: !CI,
+    command: 'npm run dev --prefix ../multiplayer-template/client',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
     timeout: 30_000,
   },
   use: {
-    baseURL: CI ? 'http://localhost:4173' : 'http://localhost:5173',
+    baseURL: 'http://localhost:5173',
     ...devices['Desktop Chrome'],
   },
 })
